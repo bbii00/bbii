b8626dfa26845935fb4a2f79c277b07ef33e74bf
bbii00 <1520660292@qq.com>
2023-09-14 12:04:46 +0800

    zidong

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.name 'bbii00'
 git config --global user.email '1520660292@qq.com'
 git config --global http.sslverify false
 git add .
-git commit -m '自动推送'
+git commit -m zidong
 git push
